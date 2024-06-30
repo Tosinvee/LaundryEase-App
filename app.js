b20eb3df8067f80e412274e5117e1cdec1e7209b
Tosinvee <bayodeoluwatosin06@gmail.com>
2024-06-30 16:57:37 +0100

    creating user model

--- app.js
+++ app.js
@@ -1,10 +1,22 @@
 const express = require("express");
 const morgan = require("morgan");
+const mongoose = require("mongoose");
 require("dotenv").config();
 
 const app = express();
 
-app.use = morgan("dev");
+app.use(morgan("dev"));
+
+const uri = process.env.MONGODB_URL;
+
+mongoose
+  .connect(uri)
+  .then(() => {
+    console.log("Database connected sucessfully");
+  })
+  .catch((error) => {
+    console.log("error connecting to database:", error);
+  });
 
 app.get("/", (req, res) => {
   res.send("Hello Tosin");
--- src/model/userModel.js
+++ src/model/userModel.js
@@ -1,1 +1,30 @@
 //////user model
+const mongoose = require("mongoose");
+
+const userSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: [true, "user must provide a name"],
+  },
+
+  email: {
+    type: String,
+    required: [true, "please provide your email"],
+    unique: true,
+    lowercase: true,
+    validate: [validator.isEmail, "please provide a valid email"],
+  },
+
+  password: {
+    type: String,
+    required: [true, "user must provide a password"],
+    minlenght: 8,
+  },
+
+  confirmPassword: {
+    type: String,
+    required: [true, "please provide your confirm email"],
+  },
+});
+const User = mongoose.model("User", userSchema);
+module.exports = User;
